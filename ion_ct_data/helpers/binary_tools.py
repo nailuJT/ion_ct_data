22026dc0db5ac943db18e6ba43e6cb9b3ed43c2c
Julian Titze <mail@julian-titze.de>
2024-04-05 12:49:39 +0200

    [CODE] Added binary tools to read phantom data

---
+++ ion_ct_data/helpers/binary_tools.py
@@ -0,0 +1,57 @@
+import numpy as np
+import struct
+import matplotlib.pyplot as plt
+import os
+from helpers.plotting import compare_images
+
+
+def read_binary(path, shape_x, shape_y, slices):
+    """
+    Reads a binary file and returns a numpy array with shape (slices, shape_x, shape_y).
+
+    :param path: The path to the binary file.
+    :param shape_x: The size of the x dimension of the output array.
+    :param shape_y: The size of the y dimension of the output array.
+    :param slices: The number of slices in the output array.
+    :return: A numpy array with the unpacked binary data.
+    """
+    size_slice = shape_x * shape_y
+    binary_format = '<' + str(size_slice * slices) + 'f'
+
+    with open(path, 'rb') as f:
+        binary = f.read()
+        image_flatt = struct.unpack(binary_format, binary)
+        image_array = np.reshape(image_flatt, (slices, shape_x, shape_y))
+
+    return image_array
+
+
+def test_compare_images():
+    path = "/home/j/J.Titze/Projects/XCAT_data/Phantoms/test3_atn_1.bin"
+    data = read_binary(path, 256, 256, 150)
+    data = data[20,:,:]
+    compare_images(data, data)
+
+
+def test_read_binary():
+    path = "/home/j/J.Titze/Projects/XCAT_data/Phantoms/test3_atn_1.bin"
+    data = read_binary(path, 256, 256, 150)
+    print(data.shape)
+    plt.imshow(data[20,:,:])
+    plt.show()
+
+
+def main():
+    BASE_PATH = "/home/j/J.Titze/Projects/XCAT_data/Phantoms/"
+    path_original = os.path.join(BASE_PATH, "test_baseline_atn_1.bin")
+    path_warped = os.path.join(BASE_PATH, "test_skin_atn_1.bin")
+
+    image_original = read_binary(path_original, 256, 256, 150)
+    image_warped = read_binary(path_warped, 256, 256, 150)
+
+    compare_images(image_original[80, :, :], image_warped[80, :, :])
+
+if __name__ == '__main__':
+    main()
+
+
